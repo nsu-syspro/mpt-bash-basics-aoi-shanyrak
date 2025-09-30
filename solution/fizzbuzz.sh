76a5ec84d349906e2e2964089b7caf88b25ec9ab
aoi-shanyrak <maxim.mezheniy@gmail.com>
2025-09-30 21:01:29 +0700

    fb2

--- solution/fizzbuzz.sh
+++ solution/fizzbuzz.sh
@@ -3,13 +3,12 @@ num="$1"
 for ((i=1; i<=$1; i++)); do
     if (( i % 15 == 0)); then
 	echo Fizz Buzz
-    fi
-    if (( i % 3 == 0 ); then
+    elif (( i % 3 == 0 )); then
 	echo Fizz
-    fi
-    if (( i % 5 == 0 )); then
+    elif (( i % 5 == 0 )); then
 	echo Buzz
-    fi 
-    echo $i
+    else
+        echo $i
+    fi
 done
 		
